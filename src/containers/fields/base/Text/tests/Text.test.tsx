df58713c7046e64d1e849e16c28cac34eac2db2b
Nafiz Ahmed <nafiz@mpower-social.com>
2019-10-16 12:23:35 +0600

    Add Text component tests

---
+++ src/containers/fields/base/Text/tests/Text.test.tsx
@@ -0,0 +1,47 @@
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+import { Provider } from 'react-redux';
+import Text from '../';
+import store from '../../../../../store';
+import { assignFieldValueAction } from '../../../../../store/ducks/formState';
+import { textFieldElement1 } from './fixtures';
+
+describe('containers/fields/base/Text', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    const mock: any = jest.fn();
+    const props = {
+      assignFieldValueActionCreator: mock,
+      fieldElement: textFieldElement1,
+      fieldValue: 'hello',
+    };
+    shallow(
+      <Provider store={store}>
+        <Text {...props} />
+      </Provider>
+    );
+  });
+
+  it('renders correctly', () => {
+    const mock: any = jest.fn();
+    const props = {
+      assignFieldValueActionCreator: mock,
+      fieldElement: textFieldElement1,
+      fieldValue: 'hello',
+    };
+    store.dispatch(
+      assignFieldValueAction(props.fieldElement.name, props.fieldValue)
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <Text {...props} />
+      </Provider>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
---
+++ src/containers/fields/base/Text/tests/fixtures.ts
@@ -0,0 +1,6 @@
+export const textFieldElement1 = {
+  bind: { required: 'Yes' },
+  label: { English: 'Name of the event' },
+  name: 'event_name',
+  type: 'text',
+};
