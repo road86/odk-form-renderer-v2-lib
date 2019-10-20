d219246bc8d724e62d666560109a9c8485f71bcf
Nafiz Ahmed <nafiz@mpower-social.com>
2019-10-20 13:39:34 +0600

    Modify BaseTypeEvalutors to include fieldParentTreeName

--- src/components/typeEvalutors/Base/index.tsx
+++ src/components/typeEvalutors/Base/index.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { TEXT_FIELD_TYPE } from '../../../constants';
 import Text from '../../../containers/fields/base/Text';
 
+/** type of fieldParentTreeName */
+export type FieldParentTreeName = string;
+
 /** interface for kobo field element */
 export interface FieldElement {
   name: string;
@@ -14,22 +17,32 @@ export interface FieldElement {
 /** props interface for BaseTypeEvaluator component */
 export interface BaseTypeEvaluatorProps {
   fieldElement: FieldElement;
+  fieldParentTreeName: FieldParentTreeName;
 }
 
 class BaseTypeEvaluator extends React.Component<BaseTypeEvaluatorProps> {
   public render() {
-    const { fieldElement } = this.props;
-    return this.typeEvaluator(fieldElement);
+    const { fieldElement, fieldParentTreeName } = this.props;
+    return this.typeEvaluator(fieldElement, fieldParentTreeName);
   }
 
   /** returns jsx components based on field types
    * @param {FieldElement} fieldElement - the field element object
+   * @param {FieldParentTreeName} fieldParentTreeName - the field parent hierchical name
    * @return {React.ReactElement} - jsx base components
    */
-  private typeEvaluator(fieldElement: FieldElement): React.ReactElement {
+  private typeEvaluator(
+    fieldElement: FieldElement,
+    fieldParentTreeName: FieldParentTreeName
+  ): React.ReactElement {
     switch (fieldElement.type) {
       case TEXT_FIELD_TYPE:
-        return <Text fieldElement={fieldElement} />;
+        return (
+          <Text
+            fieldElement={fieldElement}
+            fieldParentTreeName={fieldParentTreeName}
+          />
+        );
       default:
         return (
           <div>
