f47d57423fdd51169cd982548e1d969ca84c5b3d
ashrakrahman <lipu@mpower-social.com>
2019-10-24 17:48:07 +0600

    Add Test files for Integer Container

---
+++ src/containers/fields/base/Integer/tests/fixtures.ts
@@ -0,0 +1,7 @@
+export const integerFieldElement1 = {
+  hint:
+    'integer type with ex:change.uw.android.BREATHCOUNT appearance (can use other external apps)',
+  label: 'Ex integer widget',
+  name: 'ex_integer_widget',
+  type: 'integer',
+};
