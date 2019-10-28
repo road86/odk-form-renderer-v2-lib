a3392fc27eb8bc5fc28fb8a89039ac8c8a8cd025
ashrakrahman <lipu@mpower-social.com>
2019-10-28 18:32:22 +0600

    Integer container fixture file modified

--- src/containers/fields/base/Integer/tests/fixtures.ts
+++ src/containers/fields/base/Integer/tests/fixtures.ts
@@ -1,7 +1,12 @@
 export const integerFieldElement1 = {
-  hint:
-    'integer type with ex:change.uw.android.BREATHCOUNT appearance (can use other external apps)',
-  label: 'Ex integer widget',
-  name: 'ex_integer_widget',
+  bind: {
+    constraint: '.>=0',
+    'jr:constraintMsg': { English: 'Negative value not allowed' },
+    readonly: 'true',
+    required: 'Yes',
+  },
+  hint: 'integer type with no appearance',
+  label: 'Integer widget',
+  name: 'integer_widget',
   type: 'integer',
 };
