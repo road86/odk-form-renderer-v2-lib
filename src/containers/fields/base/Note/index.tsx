756995bb459b9f4bcd7ad754aa018afec8917027
ashrakrahman <lipu@mpower-social.com>
2019-11-05 14:26:04 +0600

    Update Note container

--- src/containers/fields/base/Note/index.tsx
+++ src/containers/fields/base/Note/index.tsx
@@ -1,26 +1,168 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { FormGroup, FormText, Label } from 'reactstrap';
-import { FieldElement } from '../../../../components/typeEvalutors/Base';
-import { getFieldHintText, getFieldLabelText } from '../../../../utils/helpers';
+import { Store } from 'redux';
+import {
+  FieldElement,
+  FieldParentTreeName,
+} from '../../../../components/typeEvalutors/Base';
+import { REQUIRED_FIELD_MSG, REQUIRED_SYMBOL } from '../../../../constants';
+import {
+  addErrorInputId,
+  assignFieldValueAction,
+  getEvaluatedExpression,
+  getFieldValue,
+  isPresentInError,
+  removeErrorInputId,
+} from '../../../../store/ducks/formState';
+import {
+  getConstraintLabelText,
+  getFieldHintText,
+  getFieldLabelText,
+  isInputRequired,
+  shouldComponentBeRelevant,
+  shouldInputViolatesConstraint,
+} from '../../../../utils/helpers';
 
+/** props interface for the text component */
 export interface NoteProps {
   fieldElement: FieldElement;
-  fieldParentTreeName: string;
+  fieldParentTreeName: FieldParentTreeName;
+  fieldValue: string;
+  assignFieldValueActionCreator: typeof assignFieldValueAction;
+  getEvaluatedExpressionSelector: any;
+  isPresentInErrorSelector: any;
+  isComponentRender: boolean;
+  addErrorInputIdActionCreator: typeof addErrorInputId;
+  removeErrorInputIdActionCreator: typeof removeErrorInputId;
   defaultLanguage: string;
 }
 
 class Note extends React.Component<NoteProps> {
   public render() {
-    const { fieldElement, defaultLanguage } = this.props;
+    const {
+      fieldElement,
+      fieldParentTreeName,
+      fieldValue,
+      isComponentRender,
+      getEvaluatedExpressionSelector,
+      isPresentInErrorSelector,
+      defaultLanguage,
+    } = this.props;
+    const isRequired = isInputRequired(fieldElement);
+    const isRequiredViolated = isRequired && (!fieldValue || fieldValue === '');
+    const isConstraintViolated =
+      fieldValue &&
+      fieldValue !== '' &&
+      shouldInputViolatesConstraint(
+        fieldElement,
+        fieldParentTreeName,
+        getEvaluatedExpressionSelector
+      );
     const fieldLabel = getFieldLabelText(fieldElement, defaultLanguage);
-    const fieldHint = getFieldHintText(fieldElement, defaultLanguage);
-    return (
-      <FormGroup>
-        <Label>{fieldLabel}</Label>
-        <FormText>{fieldHint}</FormText>
-      </FormGroup>
+    const constraintLabel = getConstraintLabelText(
+      fieldElement,
+      defaultLanguage
     );
+    if (isComponentRender) {
+      if (fieldValue == null && 'default' in fieldElement) {
+        this.props.assignFieldValueActionCreator(
+          fieldElement.name,
+          fieldElement.default
+        );
+      }
+      const fieldHint = getFieldHintText(fieldElement, defaultLanguage);
+      if (
+        (isRequiredViolated || isConstraintViolated) &&
+        !isPresentInErrorSelector(fieldParentTreeName + fieldElement.name)
+      ) {
+        this.props.addErrorInputIdActionCreator(
+          fieldParentTreeName + fieldElement.name
+        );
+      } else if (
+        !isRequiredViolated &&
+        !isConstraintViolated &&
+        isPresentInErrorSelector(fieldParentTreeName + fieldElement.name)
+      ) {
+        this.props.removeErrorInputIdActionCreator(
+          fieldParentTreeName + fieldElement.name
+        );
+      }
+      return (
+        <FormGroup>
+          <Label>{fieldLabel}</Label>
+          {isRequired && <Label>{REQUIRED_SYMBOL}</Label>}
+          <FormText>{fieldHint}</FormText>
+          {isRequiredViolated && <Label>{REQUIRED_FIELD_MSG}</Label>}
+          {isConstraintViolated && <Label>{constraintLabel}</Label>}
+        </FormGroup>
+      );
+    } else {
+      if (fieldValue != null) {
+        this.props.assignFieldValueActionCreator(fieldElement.name, null);
+        if (isPresentInErrorSelector(fieldParentTreeName + fieldElement.name)) {
+          this.props.removeErrorInputIdActionCreator(
+            fieldParentTreeName + fieldElement.name
+          );
+        }
+      }
+      return null;
+    }
   }
 }
 
-export default Note;
+/** connect the component to the store */
+
+/** Interface to describe props from mapStateToProps */
+interface DispatchedStateProps {
+  fieldValue: string;
+  getEvaluatedExpressionSelector: any;
+  isComponentRender: boolean;
+  isPresentInErrorSelector: any;
+}
+
+/** Interface to describe props from parent */
+interface ParentProps {
+  fieldElement: FieldElement;
+  fieldParentTreeName: FieldParentTreeName;
+}
+
+/** Map props to state  */
+const mapStateToProps = (
+  state: Partial<Store>,
+  parentProps: ParentProps
+): DispatchedStateProps => {
+  const { fieldElement, fieldParentTreeName } = parentProps;
+  const getEvaluatedExpressionSelector = (
+    expression: string,
+    fieldTreeName: string
+  ) => getEvaluatedExpression(state, expression, fieldTreeName);
+  const isPresentInErrorSelector = (fieldTreeName: string) =>
+    isPresentInError(state, fieldTreeName);
+  const result = {
+    fieldValue: getFieldValue(state, fieldElement.name),
+    getEvaluatedExpressionSelector,
+    isComponentRender: shouldComponentBeRelevant(
+      fieldElement,
+      fieldParentTreeName,
+      getEvaluatedExpressionSelector
+    ),
+    isPresentInErrorSelector,
+  };
+  return result;
+};
+
+/** map props to actions */
+const mapDispatchToProps = {
+  addErrorInputIdActionCreator: addErrorInputId,
+  assignFieldValueActionCreator: assignFieldValueAction,
+  removeErrorInputIdActionCreator: removeErrorInputId,
+};
+
+/** connect Note component to the redux store */
+const ConnectedNote = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Note);
+
+export default ConnectedNote;
