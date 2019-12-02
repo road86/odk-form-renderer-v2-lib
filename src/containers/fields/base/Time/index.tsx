52a7bd5639e20dd998e2564db1bfb44bb318a490
ashrakrahman <lipu@mpower-social.com>
2019-12-02 16:28:25 +0600

    Add Time Container

---
+++ src/containers/fields/base/Time/index.tsx
@@ -0,0 +1,210 @@
+import * as React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { connect } from 'react-redux';
+import { FormGroup, Label } from 'reactstrap';
+import { Store } from 'redux';
+import {
+  FieldElement,
+  FieldParentTreeName,
+} from '../../../../components/typeEvalutors/Base';
+import { REQUIRED_FIELD_MSG, REQUIRED_SYMBOL } from '../../../../constants';
+import {
+  addErrorInputId,
+  assignFieldValueAction,
+  getEvaluatedExpression,
+  getFieldValue,
+  isPresentInError,
+  removeErrorInputId,
+} from '../../../../store/ducks/formState';
+import {
+  customizeLabelsWithPreviousInputs,
+  getConstraintLabelText,
+  getFieldLabelText,
+  isInputRequired,
+  shouldComponentBeReadOnly,
+  shouldComponentBeRelevant,
+  shouldInputViolatesConstraint,
+} from '../../../../utils/helpers';
+
+/** props interface for the KbTime component */
+export interface TimeProps {
+  fieldElement: FieldElement;
+  fieldParentTreeName: FieldParentTreeName;
+  fieldValue: any;
+  assignFieldValueActionCreator: typeof assignFieldValueAction;
+  getEvaluatedExpressionSelector: any;
+  isComponentRender: boolean;
+  isPresentInErrorSelector: any;
+  addErrorInputIdActionCreator: typeof addErrorInputId;
+  removeErrorInputIdActionCreator: typeof removeErrorInputId;
+  defaultLanguage: string;
+}
+
+class KbTime extends React.Component<TimeProps> {
+  public render() {
+    const {
+      fieldElement,
+      fieldParentTreeName,
+      fieldValue,
+      isComponentRender,
+      getEvaluatedExpressionSelector,
+      isPresentInErrorSelector,
+      defaultLanguage,
+    } = this.props;
+    const isRequired = isInputRequired(fieldElement);
+    const isRequiredViolated = isRequired && (!fieldValue || fieldValue === '');
+    const isConstraintViolated =
+      fieldValue &&
+      fieldValue !== '' &&
+      shouldInputViolatesConstraint(
+        fieldElement,
+        fieldParentTreeName,
+        getEvaluatedExpressionSelector
+      );
+    const fieldLabel = getFieldLabelText(fieldElement, defaultLanguage);
+    const modifiedFieldLabel = customizeLabelsWithPreviousInputs(
+      getEvaluatedExpressionSelector,
+      fieldLabel,
+      fieldParentTreeName + fieldElement.name
+    );
+    const constraintLabel = getConstraintLabelText(
+      fieldElement,
+      defaultLanguage
+    );
+    const modifiedConstraintLabel = customizeLabelsWithPreviousInputs(
+      getEvaluatedExpressionSelector,
+      constraintLabel,
+      fieldParentTreeName + fieldElement.name
+    );
+    if (isComponentRender) {
+      if (fieldValue == null && 'default' in fieldElement) {
+        this.props.assignFieldValueActionCreator(
+          fieldParentTreeName + fieldElement.name,
+          fieldElement.default
+        );
+      }
+      const isReadonly = shouldComponentBeReadOnly(
+        fieldElement,
+        fieldParentTreeName,
+        getEvaluatedExpressionSelector
+      );
+      if (
+        (isRequiredViolated || isConstraintViolated) &&
+        !isPresentInErrorSelector(fieldParentTreeName + fieldElement.name)
+      ) {
+        this.props.addErrorInputIdActionCreator(
+          fieldParentTreeName + fieldElement.name
+        );
+      } else if (
+        !isRequiredViolated &&
+        !isConstraintViolated &&
+        isPresentInErrorSelector(fieldParentTreeName + fieldElement.name)
+      ) {
+        this.props.removeErrorInputIdActionCreator(
+          fieldParentTreeName + fieldElement.name
+        );
+      }
+
+      return (
+        <FormGroup>
+          <Label>{modifiedFieldLabel}</Label>
+          {isRequired && <Label>{REQUIRED_SYMBOL}</Label>}
+          <DatePicker
+            name={fieldElement.name}
+            selected={fieldValue ? fieldValue : null}
+            onChange={this.handleChange(fieldElement.name)}
+            showTimeSelect={true}
+            showTimeSelectOnly={true}
+            timeFormat="h:m aa"
+            timeIntervals={15}
+            timeCaption="time"
+            dateFormat="h:mm aa"
+            placeholderText="h:m aa"
+            className="form-control"
+            readOnly={isReadonly}
+          />
+
+          {isRequiredViolated && <Label>{REQUIRED_FIELD_MSG}</Label>}
+          {isConstraintViolated && <Label>{modifiedConstraintLabel}</Label>}
+        </FormGroup>
+      );
+    } else {
+      if (fieldValue != null) {
+        this.props.assignFieldValueActionCreator(
+          fieldParentTreeName + fieldElement.name,
+          null
+        );
+        if (isPresentInErrorSelector(fieldParentTreeName + fieldElement.name)) {
+          this.props.removeErrorInputIdActionCreator(
+            fieldParentTreeName + fieldElement.name
+          );
+        }
+      }
+      return null;
+    }
+  }
+
+  private handleChange = (name: any) => (value: any) => {
+    this.props.assignFieldValueActionCreator(
+      this.props.fieldParentTreeName + name,
+      value !== '' ? value : null
+    );
+  };
+}
+
+/** connect the component to the store */
+
+/** Interface to describe props from mapStateToProps */
+interface DispatchedStateProps {
+  fieldValue: any;
+  getEvaluatedExpressionSelector: any;
+  isComponentRender: boolean;
+  isPresentInErrorSelector: any;
+}
+
+/** Interface to describe props from parent */
+interface ParentProps {
+  fieldElement: FieldElement;
+  fieldParentTreeName: FieldParentTreeName;
+}
+
+/** Map props to state  */
+const mapStateToProps = (
+  state: Partial<Store>,
+  parentProps: ParentProps
+): DispatchedStateProps => {
+  const { fieldElement, fieldParentTreeName } = parentProps;
+  const getEvaluatedExpressionSelector = (
+    expression: string,
+    fieldTreeName: string
+  ) => getEvaluatedExpression(state, expression, fieldTreeName);
+  const isPresentInErrorSelector = (fieldTreeName: string) =>
+    isPresentInError(state, fieldTreeName);
+  const result = {
+    fieldValue: getFieldValue(state, fieldParentTreeName + fieldElement.name),
+    getEvaluatedExpressionSelector,
+    isComponentRender: shouldComponentBeRelevant(
+      fieldElement,
+      fieldParentTreeName,
+      getEvaluatedExpressionSelector
+    ),
+    isPresentInErrorSelector,
+  };
+  return result;
+};
+
+/** map props to actions */
+const mapDispatchToProps = {
+  addErrorInputIdActionCreator: addErrorInputId,
+  assignFieldValueActionCreator: assignFieldValueAction,
+  removeErrorInputIdActionCreator: removeErrorInputId,
+};
+
+/** connect KbTime component to the redux store */
+const ConnectedTime = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(KbTime);
+
+export default ConnectedTime;
