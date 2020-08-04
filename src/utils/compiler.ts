ec432a8267632f0110fc8eb60d053e261b051a54
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-08-04 19:27:09 +0600

    Add doc for kbPosition.

--- src/utils/compiler.ts
+++ src/utils/compiler.ts
@@ -639,6 +639,13 @@ function kbCoalesce(funcName: any, params: any, _paramsTokens: any) {
   return [false, null];
 }
 
+/**
+ * kbPosition parses the function position and returns functionParseReturnObject
+ * @param funcName - the function name of the token
+ * @param params - calculated value of the params tokens
+ * @param _paramsTokens - orginal param tokens
+ * @returns functionParseReturnObject
+ */
 // tslint:disable-next-line: variable-name
 function kbPosition(funcName: any, _params: any, _paramsTokens: any) {
   // tslint:disable-next-line: triple-equals
