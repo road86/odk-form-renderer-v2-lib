80a5cba805e0329a68e7a8717df14dff8b53af5b
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-08-04 19:02:06 +0600

    Add doc for skipWhiteSpace.

--- src/utils/compiler.ts
+++ src/utils/compiler.ts
@@ -422,7 +422,12 @@ function tokenizeDiv(input: any, current: any) {
 function tokenizeName(input: any, current: any) {
   return tokenizePattern('name', /[a-z_]/i, input, current);
 }
-
+/**
+ * skipWhiteSpace removes space and iterates the position of compiler
+ * @param {any} input - the input expression, valid cases involve only string type
+ * @param {any} current - the position of the compiler at the input expression
+ * @returns tokenizeReturnObject that consumes characters without returning type
+ */
 function skipWhiteSpace(input: any, current: any) {
   return /\s/.test(input[current]) ? [1, null] : [0, null];
 }
