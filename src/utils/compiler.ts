504b77e86c25766f82022520debf7332af344892
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-08-04 20:29:47 +0600

    Add doc for evaluater.

--- src/utils/compiler.ts
+++ src/utils/compiler.ts
@@ -1751,6 +1751,14 @@ function parser(leftOutput: any, tokens: any, pos: any) {
   return output;
 }
 
+/**
+ * odk wrapper evaluator, default method that will be exported, parses expressions
+ * @param {any} expression - expression that needs to be evaluated
+ * @param {any} tmpUserInput - the variable key-value json, (current user input json)
+ * @param {any} tmpFormItemProperty - the name key-value json (current option list)
+ * @param {any} tmpCurrentHierarchicalName - the node at which the evaluater is called
+ * @returns evaluated value
+ */
 export default function evaluater(
   expression: string,
   tmpUserInput: any,
