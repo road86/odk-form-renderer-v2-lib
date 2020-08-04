0bbc7fe4d0dd498bd370daa3ead401b98fe3ef4f
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-08-04 14:07:51 +0600

    Add doc to tokenizeParenOpen.

--- src/utils/compiler.ts
+++ src/utils/compiler.ts
@@ -15,6 +15,12 @@ function tokenizeCharacter(type: any, value: any, input: any, current: any) {
   return value === input[current] ? [1, { type, value }] : [0, null];
 }
 
+/**
+ * tokenizeParenOpen returns tokenizeReturnObject for character (
+ * @param {any} input - the input expression, valid cases involve only string type
+ * @param {any} current - the position of the compiler at the input expression
+ * @returns - tokenizeReturnObject
+ */
 function tokenizeParenOpen(input: any, current: any) {
   return tokenizeCharacter('paren', '(', input, current);
 }
